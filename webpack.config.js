832619b484e40d4647efc42adfd2cb65e720e552
certilremy <certilremy@gmail.com>
2020-05-11 18:02:21 -0400

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  watch: true,
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
   },
 };
