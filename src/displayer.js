30e1b762fa532749ba445c4876d401e7b1f671ea
certilremy <certilremy@gmail.com>
2020-05-12 09:24:02 -0400

    change weather display style

--- src/displayer.js
+++ src/displayer.js
@@ -1,20 +1,25 @@
+import utils from './utils';
+
 const displayer = (() => {
   const applicationHelp = document.getElementById('application-help');
   const cityname = document.getElementById('city-name');
+  const weatherSkyInfo = document.getElementById('weather-sky-info');
+  const cloudIndicator = document.getElementById('cloud-indicator');
   const temp = document.getElementById('temp');
   const tempMax = document.getElementById('temp_max');
   const tempMin = document.getElementById('temp_min');
-  const weatherDesct = document.getElementById('weather-desc');
+  // const weatherDesct = document.getElementById('weather-desc');
   const wendSpeed = document.getElementById('wind-speed');
   const displayCityData = (data) => {
     console.log(data);
     applicationHelp.style.display = 'none';
     cityname.innerHTML = `Weather for ${data.name} country code ${data.sys.country} `;
-    weatherDesct.innerHTML = `Weather info: ${data.weather[0].description}`;
+    weatherSkyInfo.innerHTML = `${data.weather[0].description}`;
     temp.innerHTML = `Temperature: ${data.main.temp}`;
     tempMax.innerHTML = `Temperature max: ${data.main.temp_max}`;
     tempMin.innerHTML = `Temperature min: ${data.main.temp_min}`;
     wendSpeed.innerHTML = `Wind speed: ${data.wind.speed}`;
+    cloudIndicator.innerHTML = utils.displayIconForCondition(data);
   };
 
   const displayError = () => {
