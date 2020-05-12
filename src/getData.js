c810f9f8ec51c87309b8173a9ec7b4fe60050081
certilremy <certilremy@gmail.com>
2020-05-11 23:09:12 -0400

    add get weather fahrenheit

--- src/getData.js
+++ src/getData.js
@@ -7,7 +7,13 @@ const getData = (() => {
     handleError.ApiErrHandler(cityData);
   };
 
-  return { getWeatherCelcius };
+  const getWeatherFahrenheit = async (city) => {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=fdd680c9db12c88b556332cfd0c4de11`, { mode: 'cors' });
+    const cityData = await response.json();
+    handleError.ApiErrHandler(cityData);
+  };
+
+  return { getWeatherCelcius, getWeatherFahrenheit };
 })();
 
 export default getData;
